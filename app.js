94ee7c5c45baca6059d7a5b474de67a10510cf57
Cloud next door <cloudnextdoor@gmail.com>
2022-11-12 22:21:43 -0600

    changed the message in the app.js file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Smile for the world!'));
+app.get('/', (req, res) => res.send('Only when I smile when the world sees the beauty in my heart!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
